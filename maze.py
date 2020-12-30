eaa740d40364faafd7b5968ec6ac249b59bf644d
murnant <74027673+murnant@users.noreply.github.com>
2020-12-30 14:41:32 -0600

    update

---
+++ maze.py
@@ -0,0 +1,50 @@
+import pygame
+import random
+
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode ((1000, 500),0,32)
+
+pygame.init()
+player_hitbox = pygame.Rect(100, 100,10,10)
+zom = 0
+#maze_pic = pygame.image.load("../assets/maze.png")
+#maze_pic_small = pygame.transform.scale(maze_pic, (5000, 2500))
+walls =[]
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((100,100,100))
+
+
+    #screen.blit(maze_pic_small, (x, y))
+
+
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]:
+        started = True
+    if pygame.mouse.get_pressed()[0]:
+        walls.append(pygame.Rect(mouse_x, mouse_y,10,10))
+    for w in walls:
+        if keys[pygame.K_RIGHT]:
+            w.x -= 1
+        if keys[pygame.K_LEFT]:
+            w.x += 1
+        if keys[pygame.K_UP]:
+            w.y += 1
+        if keys[pygame.K_DOWN]:
+            w.y -= 1
+
+
+        w.width += zom
+        w.height += zom
+        w.x += zom
+        w.y += zom
+
+                
+        pygame.draw.rect(screen, (255, 0, 0), w)
+
+    pygame.draw.rect(screen, (255, 255, 255), player_hitbox)
+
+    pygame.display.update() # finally pushes changes to the screen
