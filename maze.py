968932363eb8d0e74f738397027c1a32dd8a3d83
murnant <74027673+murnant@users.noreply.github.com>
2020-12-30 15:23:23 -0600

    update

--- maze.py
+++ maze.py
@@ -6,11 +6,12 @@ screen = pygame.display.set_mode ((1000, 500),0,32)
 
 pygame.init()
 player_hitbox = pygame.Rect(100, 100,10,10)
-zom = 0
+zom = 50
+running = True
 #maze_pic = pygame.image.load("../assets/maze.png")
 #maze_pic_small = pygame.transform.scale(maze_pic, (5000, 2500))
 walls =[]
-while True:
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -31,18 +32,19 @@ while True:
         walls.append(pygame.Rect(mouse_x, mouse_y,10 +zom,10 +zom))
     for w in walls:
         if keys[pygame.K_RIGHT]:
-            w.x -= 1
+            w.x -= 3
         if keys[pygame.K_LEFT]:
-            w.x += 1
+            w.x += 3
         if keys[pygame.K_UP]:
-            w.y += 1
+            w.y += 3
         if keys[pygame.K_DOWN]:
-            w.y -= 1
+            w.y -= 3
 
 
         w.width = 10 +zom
         w.height = 10 +zom
-
+        if w.colliderect(player_hitbox):
+            running=False
                 
         pygame.draw.rect(screen, (255, 0, 0), w)
 
