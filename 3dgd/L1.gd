8b14fcae0f8a7c57ede61fe332d6c3bf895cc9a0
murnant <74027673+murnant@users.noreply.github.com>
2021-01-15 17:50:26 -0600

    update

--- 3dgd/L1.gd
+++ 3dgd/L1.gd
@@ -2,10 +2,14 @@ extends Spatial
 func _process(delta):
 	if Input.is_action_pressed('right'):
 		$camerahub.rotate_y(0.005)
+		$player.rotate_y(0.005)
 	if Input.is_action_pressed('left'):
 		$camerahub.rotate_y(-0.005)
+		$player.rotate_y(-0.005)
 	if Input.is_action_pressed('up'):
 		$camerahub.rotate_x(-0.005)
 	if Input.is_action_pressed('down'):
 		$camerahub.rotate_x(0.005)
 	$sun.rotate_x(0.01)
+	$ball_spinner.rotate_y(0.01)
+	
--- 3dgd/player.gd
+++ 3dgd/player.gd
@@ -10,16 +10,16 @@ func _physics_process(delta):
 	velocity += gravity * delta
 	get_imput()
 	velocity = move_and_slide(velocity, Vector3.UP)
-	
 func get_imput():
-	velocity.x = 0
-	velocity.z = 0
+	var vy = velocity.y
+	velocity = Vector3()
 	if Input.is_action_pressed('d'):
-		velocity.x += speed
+		velocity.x += transform.basis.x * speed
 	if Input.is_action_pressed('a'):
-		velocity.x -= speed
+		velocity.x -= transform.basis.x * speed
 	if Input.is_action_pressed('s'):
-		velocity.z += speed
+		velocity.z += transform.basis.z * speed
 	if Input.is_action_pressed('w'):
-		velocity.z -= speed
-		
+		velocity.z -= transform.basis.z * speed
+	velocity.y = vy
+	
