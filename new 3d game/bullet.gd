72f36196506b9ff0557d3c3d27c0bbd637dde50e
murnant <74027673+murnant@users.noreply.github.com>
2021-01-17 11:33:31 -0600

    update

--- new 3d game/bullet.gd
+++ new 3d game/bullet.gd
@@ -2,6 +2,5 @@ extends KinematicBody
 var velocity = Vector3()
 func _physics_process(delta):
 	velocity = Vector3()
-	#velocity += global_transform.basis.y * 6.0
-	velocity += -global_transform.basis.z * 6.0
+	velocity += -global_transform.basis.z * 9.0
 	move_and_slide(velocity)
