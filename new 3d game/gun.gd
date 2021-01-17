4d873edf55d9429d3a3024f2f2d2c67d966690da
murnant <74027673+murnant@users.noreply.github.com>
2021-01-17 11:20:48 -0600

    update

--- new 3d game/gun.gd
+++ new 3d game/gun.gd
@@ -1,7 +1,10 @@
 extends StaticBody
 const BULLET = preload("bullet.tscn")
+var timer = 0
 func _process(delta):
-	if Input.is_action_pressed("space"):
+	timer -= 1
+	if Input.is_action_pressed("space") and timer <= 0:
 		var bullet = BULLET.instance()
 		bullet.global_transform = $Position3D.global_transform
 		get_node("/root").add_child(bullet)
+		timer = 50
