a3af1d6b2809bd6aed5e6c909fd51a0b320cb0a9
murnant <74027673+murnant@users.noreply.github.com>
2021-01-20 16:39:15 -0600

    update

--- bryce's game/player.gd
+++ bryce's game/player.gd
@@ -2,13 +2,13 @@ extends KinematicBody
 var velocity = Vector3()
 func _process(delta):
 	if Input.is_action_pressed('right'):
-		rotate_y(-0.04)
+		rotate_y(-0.01)
 	if Input.is_action_pressed('left'):
-		rotate_y(0.04)
+		rotate_y(0.01)
 	if Input.is_action_pressed('up'):
-		$t.rotate_x(0.04)
+		$t.rotate_x(0.01)
 	if Input.is_action_pressed('down'):
-		$t.rotate_x(-0.04)
+		$t.rotate_x(-0.01)
 	if Input.is_action_pressed("f_or_j"):
 		velocity = -global_transform.basis.z * 7
 		move_and_slide(velocity)
