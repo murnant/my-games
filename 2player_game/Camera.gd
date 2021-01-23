bf3ccbe15089ac011ff682697ec589c847d1126b
murnant <74027673+murnant@users.noreply.github.com>
2021-01-23 11:29:13 -0600

    update

--- 2player_game/Camera.gd
+++ 2player_game/Camera.gd
@@ -1,6 +1,6 @@
 extends Spatial
 func _process(delta):
 	if Input.is_action_pressed('c_up'):
-		rotate_x(-0.005)
-	if Input.is_action_pressed('c_down'):
 		rotate_x(0.005)
+	if Input.is_action_pressed('c_down'):
+		rotate_x(-0.005)
