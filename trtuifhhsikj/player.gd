199e07094d4361f4506a51def9c4bc575c5f344c
murnant <74027673+murnant@users.noreply.github.com>
2021-01-31 17:32:46 -0600

    update

--- trtuifhhsikj/player.gd
+++ trtuifhhsikj/player.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 var velocity = Vector2()
 export var speed = 100
+var slash = false
+var slash_timer = 75
 func get_input():
 	velocity = Vector2()
 	if Input.is_action_pressed('right'):
@@ -11,8 +13,22 @@ func get_input():
 		velocity.y = speed
 	if Input.is_action_pressed('up'):
 		velocity.y = -speed
+	if Input.is_action_pressed("space"):
+		slash = true
+	if slash:
+		slash_timer -= 1
+		if slash_timer <= 0:
+			#slash = false
+			if slash_timer >= -73:
+				rotate(-0.01)
+			else:
+				slash = false
+				slash_timer = 75
+		else:
+			rotate(0.01)
 func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
 func _on_Area2D_body_entered(b):
-	b.Break()
+	if slash:
+		b.Break()
