a1b2694689cedf29d16d72e6d8b64de44e09f522
murnant <74027673+murnant@users.noreply.github.com>
2021-01-31 16:42:22 -0600

    update

--- trtuifhhsikj/enemy_sponer.gd
+++ trtuifhhsikj/enemy_sponer.gd
@@ -1,6 +1,10 @@
 extends Node2D
 var ENEMY = preload('enemy.tscn')
+var timer = 200
 func _physics_process(delta):
-	var enemy = ENEMY.instance()
-	enemy.global_transform = $Position2D.global_transform
-	get_node("/root").add_child(enemy)
+	timer -= 1
+	if timer <= 0:
+		var enemy = ENEMY.instance()
+		enemy.global_transform = $Position2D.global_transform
+		get_node("/root").add_child(enemy)
+		timer = 200
