5a7076f7cefe0edb538b0a778bf8f315463f3c7f
murnant <74027673+murnant@users.noreply.github.com>
2021-02-02 15:06:02 -0600

    update

--- trtuifhhsikj/player.gd
+++ trtuifhhsikj/player.gd
@@ -4,34 +4,55 @@ export var speed = 100
 var slash = false
 var slash_timer = 75
 var slash_cooldown = 0
+var slash_right = true
 func get_input():
 	velocity = Vector2()
-	if Input.is_action_pressed('right'):
-		velocity.x = speed
-	if Input.is_action_pressed('left'):
-		velocity.x = -speed
-	if Input.is_action_pressed('down'):
-		velocity.y = speed
-	if Input.is_action_pressed('up'):
-		velocity.y = -speed
+	if not slash:
+		if Input.is_action_pressed('right'):
+			velocity.x = speed
+			slash_right = true
+		if Input.is_action_pressed('left'):
+			velocity.x = -speed
+			slash_right = false
+		if Input.is_action_pressed('down'):
+			velocity.y = speed
+		if Input.is_action_pressed('up'):
+			velocity.y = -speed
 		
 	#slashing code
-	slash_cooldown -= 1
 	if Input.is_action_pressed("space"):
 		if slash_cooldown <= 0:
 			slash = true
-	if slash:
-		slash_timer -= 1
-		if slash_timer <= 0:
-			#slash = false
-			if slash_timer >= -73:
-				rotate(-0.02)
+	slash_cooldown -= 1
+	if slash_right:
+		if slash:
+			slash_timer -= 1
+			if slash_timer <= 0:
+				#slash = false
+				if slash_timer >= -73:
+					rotate(-0.02)
+				else:
+					slash = false
+					slash_timer = 75
+					slash_cooldown = 100
 			else:
-				slash = false
-				slash_timer = 75
-				slash_cooldown = 100
-		else:
-			rotate(0.02)
+				rotate(0.02)
+	else:
+		if slash:
+			slash_timer -= 1
+			if slash_timer <= 0:
+				#slash = false
+				if slash_timer >= -73:
+					rotate(0.02)
+				else:
+					slash = false
+					slash_timer = 75
+					slash_cooldown = 100
+			else:
+				rotate(-0.02)
+
+
+
 func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
