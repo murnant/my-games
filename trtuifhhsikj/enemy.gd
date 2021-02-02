103c17c497c90500ac937f66a95de1cf2eb14a2f
murnant <74027673+murnant@users.noreply.github.com>
2021-02-01 18:58:40 -0600

    update

--- trtuifhhsikj/enemy.gd
+++ trtuifhhsikj/enemy.gd
@@ -2,7 +2,11 @@ extends KinematicBody2D
 var velocity = Vector2()
 func _physics_process(delta):
 	velocity.x = -45
+	velocity.y += 10
 	move_and_slide(velocity)
 func Break():
 	#print("346373472")
 	queue_free()
+func _on_Area2D_body_entered(b):
+	velocity.y = -850
+	move_and_slide(velocity)
