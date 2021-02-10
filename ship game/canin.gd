f2d197e684ab956f3107ec1e834a6cd1b6dc0451
murnant <74027673+murnant@users.noreply.github.com>
2021-02-10 15:15:47 -0600

    update

---
+++ ship game/canin.gd
@@ -0,0 +1,10 @@
+extends StaticBody
+var BULLET = preload('bullet.tscn')
+var timer = 0
+func _process(delta):
+	timer -= 1
+	if Input.is_action_pressed("space") and timer <= 0:
+		var bullet = BULLET.instance()
+		bullet.global_transform = $Position3D.global_transform
+		get_node("/root").add_child(bullet)
+		timer = 50
---
+++ ship game/player.gd
@@ -0,0 +1,14 @@
+extends KinematicBody
+var velocity = Vector3()
+func _process(delta):
+	if Input.is_action_pressed('right'):
+		rotate_y(-0.01)
+	if Input.is_action_pressed('left'):
+		rotate_y(0.01)
+	if Input.is_action_pressed('up'):
+		$c.rotate_x(0.01)
+	if Input.is_action_pressed('down'):
+		$c.rotate_x(-0.01)
+	if Input.is_action_pressed("f_or_j"):
+		velocity = -global_transform.basis.z * 7
+		move_and_slide(velocity)
