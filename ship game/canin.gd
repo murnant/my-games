4ab885ef491f6cd01148398e94dad4b9564d2dee
murnant <74027673+murnant@users.noreply.github.com>
2021-02-10 15:47:20 -0600

    update

--- ship game/canin.gd
+++ ship game/canin.gd
@@ -1,10 +1,10 @@
 extends StaticBody
 var BULLET = preload('bullet.tscn')
 var timer = 0
-func _process(delta):
+func _on_Area_body_entered(body):
 	timer -= 1
-	if Input.is_action_pressed("space") and timer <= 0:
-		var bullet = BULLET.instance()
-		bullet.global_transform = $Position3D.global_transform
-		get_node("/root").add_child(bullet)
-		timer = 50
+	#if Input.is_action_pressed("space") and timer <= 0:
+	var bullet = BULLET.instance()
+	bullet.global_transform = $Position3D.global_transform
+	get_node("/root").add_child(bullet)
+	timer = 50
--- ship game/player.gd
+++ ship game/player.gd
@@ -1,6 +1,18 @@
 extends KinematicBody
 var velocity = Vector3()
-func _process(delta):
+var gravity = Vector3.DOWN * 12
+var speed = 4
+var jump_speed = 1
+var spin = 0.1
+
+func _physics_process(delta):
+	velocity += gravity * delta
+	get_imput()
+	velocity = move_and_slide(velocity, Vector3.UP)
+func get_imput():
+	var vy = velocity.y
+	velocity = Vector3()
+	velocity.y = vy
 	if Input.is_action_pressed('right'):
 		rotate_y(-0.01)
 	if Input.is_action_pressed('left'):
@@ -11,4 +23,5 @@ func _process(delta):
 		$c.rotate_x(-0.01)
 	if Input.is_action_pressed("f_or_j"):
 		velocity = -global_transform.basis.z * 7
+		velocity.y = vy
 		move_and_slide(velocity)
