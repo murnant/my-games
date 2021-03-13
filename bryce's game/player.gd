6900c7ee0a6e2df2701a4b1fc7257fea350cb8fb
murnant <74027673+murnant@users.noreply.github.com>
2021-03-13 13:52:01 -0600

    update

--- bryce's game/player.gd
+++ bryce's game/player.gd
@@ -1,12 +1,21 @@
 extends KinematicBody
 var velocity = Vector3()
+var down = Vector3()
 var air = 0
-var speed = 0
+var speed = 0.1
+func die():
+	$t.hide()
+	$Crashedhelicopter.show()
 func _process(delta):
 	if Input.is_action_pressed("speed_up"):
-		speed += 0.01
+		speed += 0.001
 	if Input.is_action_pressed("speed_down"):
-		speed -= 0.01
+		if speed < 0:
+			speed -= 0.001
+		else:
+			speed = 0.01
+			down.y -= 1
+			move_and_slide(down)
 	if Input.is_action_pressed('right'):
 		rotate_y(-0.01)
 	if Input.is_action_pressed('left'):
@@ -23,10 +32,19 @@ func _process(delta):
 	velocity = $t/t.global_transform.basis.y * speed
 	velocity.z *= 50
 	velocity.x *= 50
-	velocity.y += speed
-	velocity.y -= air #* 0.01
+	if velocity.y < air:
+		down.y -= 0.01
+		move_and_slide(down)
+	else:
+		down = Vector3()
+	#velocity.y += speed
+	#velocity.y -= air #* 0
 	#velocity.y = 
 	#else:
 		#velocity = Vector3()
 	move_and_slide(velocity)
 	air += velocity.y -speed
+
+
+func _on_Area_body_entered(body):
+	die()
